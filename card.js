943ca006fa2a5517f6fc011e9e6c65d2c7da46c6
mujketa <76135739+mujketa@users.noreply.github.com>
2023-06-15 03:07:04 +0200

    Edit modal working and added refresh function after alerts

--- card.js
+++ card.js
@@ -1,28 +1,27 @@
- let cards = [];
-
- const fillEditData = (cardIdd) => {
-    const card = cards.find(card => card.id_vijesti === cardIdd);
-    const cardId = document.getElementById('card-id').value;
-    const cardNaslov = document.getElementById('card-naslov').value;
-    const cardSlika = document.getElementById('card-slika').value;
-    const cardTekst = document.getElementById('card-tekst').value;
-
-
-    cardId.value = card.id_vijesti;
-    cardNaslov.value = card.naslov;
-    cardSlika.value = card.slika;
-    cardTekst.value = card.tekst;
-}
-
+const deleteCard = (id) => {
+    if(window.confirm("Da li ste sigurni da zelite obrisati vijest ? ")){
+    fetch(`http://localhost:5000/api/remove/${id}`, {
+         method: "DELETE",
+     })
+     (alert("Vijest uspješno obrisana "), osvjeziStranicu())
+     .then(res => {
+         if(!res.ok)
+         {
+             alert('Error');
+         }
+     })
+    .catch(err => console.log(err)) 
+    }
+ }
 
  //PUT
-const editCards = (id) => { 
+const editCards = () => { 
     const cardId = document.getElementById('card-id').value;
     const cardNaslov = document.getElementById('card-naslov').value;
     const cardSlika = document.getElementById('card-slika').value;
     const cardTekst = document.getElementById('card-tekst').value;
 
-    fetch(`http://localhost:5000/api/update/${id}/`, {
+    fetch(`http://localhost:5000/api/update/${cardId}/`, {
         method: 'PUT', 
         headers: new Headers({'content-type': 'application/json'}),
         body: JSON.stringify({
@@ -30,7 +29,7 @@ const editCards = (id) => {
             naslov: cardNaslov,
             slika: cardSlika,
             tekst: cardTekst
-        })
+        }) 
     })
     .then(res => {
         if(!res.ok)
@@ -39,10 +38,12 @@ const editCards = (id) => {
         }
     })
     .catch(error => console.log(error))
+
 }
 
-
-const cardss = () => {
-    console.log(hellooo);
-}
+function osvjeziStranicu() {
+    setTimeout(function() {
+      location.reload();
+    }, 1000); // Ovdje postavite željeni broj milisekundi (npr. 5000 za 5 sekundi)
+  }
  
